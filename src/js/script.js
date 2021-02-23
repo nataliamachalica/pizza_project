d5dde347381d11adc14a02dfb0ef0d6dba3a26d1
Natalia Szlapka <n.machalica@gmail.com>
2021-02-23 22:05:04 +0100

    Add first classes

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
+
 {
   'use strict';
 
@@ -52,7 +53,63 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(id, data){
+
+      const thisProduct = this;
+
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+
+      console.log('new Product:', thisProduct);
+    }
+
+    renderInMenu(){
+      const thisProduct = this;
+
+      /* generate HTML based on templates */
+
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      //console.log('generatedHTML:', generatedHTML);
+
+      /* create element using utils.createElementFromHTML */
+
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      /* find menu container */
+
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
+      /* add element to menu */
+
+      menuContainer.appendChild(thisProduct.element);
+
+    }
+
+  }
+
+
+
   const app = {
+    initMenu: function(){
+      const thisApp = this;
+
+      console.log('thisApp.data:', thisApp.data);
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+      /*const testProduct = new Product();
+      console.log('testProduct:', testProduct);*/
+    },
+
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,6 +117,8 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
