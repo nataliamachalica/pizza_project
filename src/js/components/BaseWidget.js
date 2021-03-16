6dd009e4b01a8658a6ddeb100b8e4ea28e530bfe
Natalia Szlapka <n.machalica@gmail.com>
2021-03-16 12:25:37 +0100

    Add widgets and date and hour picker

--- src/js/components/BaseWidget.js
+++ src/js/components/BaseWidget.js
@@ -5,7 +5,55 @@ class BaseWidget{
     thisWidget.dom = {};
     thisWidget.dom.wrapper = wrapperElement;
 
-    thisWidget.value = initialValue;
+    thisWidget.correctValue = initialValue;
+  }
+
+  get value(){ // get value jest getterem czyli metodą wykonywana przyk kazdej probieodczytania właściwoxsci  value
+    const thisWidget = this;
+
+    return thisWidget.correctValue;
+  }
+
+  set value(value){ // setter - metoda wykonywana przy kazdej próbie ustawienia nowej wartosci dla właciwosci value
+    const thisWidget = this;
+
+    const newValue = thisWidget.parseValue(value);
+
+    if(newValue !== thisWidget.correctValue && thisWidget.isValid(newValue)){
+      thisWidget.correctValue = newValue;
+      thisWidget.announce();
+    }
+
+    thisWidget.renderValue();
+  }
+
+  setValue(value){
+    const thisWidget = this;
+
+    thisWidget.value = value;
+  }
+
+  parseValue(value){
+    return parseInt(value);
+  }
+
+  isValid(value){
+    return !isNaN(value);
+  }
+
+  renderValue(){
+    const thisWidget = this;
+
+    thisWidget.dom.wrapper.innerHTML = thisWidget.value;
+  }
+
+  announce(){
+    const thisWidget = this;
+
+    const event = new CustomEvent('updated', {
+      bubbles: true
+    });
+    thisWidget.dom.wrapper.dispatchEvent(event);
   }
 }
 
